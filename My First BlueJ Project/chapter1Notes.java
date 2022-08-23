cafbb0e4101bad97ba678bdcd307d541cb981fe4
Tyler-Browning <tbrowning@stu.naperville203.org>
2022-08-23 12:38:56 -0500

    Started BlueJ and chapter 1 notes

---
+++ My First BlueJ Project/chapter1Notes.java
@@ -0,0 +1,37 @@
+/**
+// Tyler Browning
+// 8/23/22
+// Notes for Chapter 1
+// Where we are going to take some code notes
+// Who helped: Mr. Hayes
+*/
+
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class chapter1Notes
+{
+    /**
+    * main method
+    * where we are going to code
+    */
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        // These are the basic data types (Math)
+        int x=9,y,z;
+        double num = 5, num1 =23.4567, num2;
+        char letter = 'd';
+        boolean choice = false;
+        long a = 9; // More memory
+        short b = 5; // Less memory
+        
+        
+        // These are objects (Object such as .len)
+        String word = new String("Tyler Browning");
+        Integer number = new Integer(47);
+        Double number2 = new Double(34.5);
+    }
+}
